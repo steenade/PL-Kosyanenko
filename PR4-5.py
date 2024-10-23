3059afa03ae79e261687737926253eff8e6505b8
steenade <steenade@mail.ru>
2024-10-23 17:44:26 +0300

    Add files via upload

---
+++ PR4-5.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+f = 1
+
+for i in range(1, n+1):
+    f *= i
+print(f)
+
+	
+
+
+
+
+
+	
+
+	
+
+	
+	
---
+++ PR4-7.py
@@ -0,0 +1,16 @@
+n = int(input())
+total = 1
+total2 = 0
+for i in range (1, n+1):
+	total*= i
+	total2+= total
+print(total2)
+
+
+
+	
+
+	
+
+	
+	
